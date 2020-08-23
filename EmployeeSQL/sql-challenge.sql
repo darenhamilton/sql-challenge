55cd24f4778f3cc07134bfd45e6091cbf549ff89
Hamilton <hamihomeme@gmail.com>
2020-08-22 17:02:48 -0700

    New sql attempt

--- EmployeeSQL/sql-challenge.sql
+++ EmployeeSQL/sql-challenge.sql
@@ -3,15 +3,15 @@
 -- Create tables
 CREATE TABLE IF NOT EXISTS departments
 (
-    dept_no VARCHAR(10) NOT NULL,
-    dept_nam VARCHAR(30) NOT NULL,
-    PRIMARY KEY(dept_no, dept_nam)
+    dept_no VARCHAR(10) NOT NULL UNIQUE,
+    dept_nam VARCHAR(50) NOT NULL,
+    PRIMARY KEY(dept_no)
 );
 
 CREATE TABLE IF NOT EXISTS employees
 (
-    employee_no INTEGER NOT NULL,
-    employee_title_id VARCHAR(30),
+    employee_no INTEGER NOT NULL UNIQUE,
+    employee_title_id VARCHAR(10) NOT NULL,
     birthdate DATE,
     first_name VARCHAR(50),
     last_name VARCHAR(50),
@@ -20,39 +20,32 @@ CREATE TABLE IF NOT EXISTS employees
     PRIMARY KEY(employee_no)
 );
 
-CREATE TABLE IF NOT EXISTS salary
+CREATE TABLE IF NOT EXISTS salarys
 (
-    employee_no INTEGER NOT NULL,
-    employees_employee_no INTEGER,
-    salary INTEGER,
+    employee_no INTEGER NOT NULL UNIQUE,
+    salary VARCHAR(10) NOT NULL,
     PRIMARY KEY(employee_no)
 );
 
 CREATE TABLE IF NOT EXISTS titles
 (
-    title_id VARCHAR(10) NOT NULL,
-    employees_employee_no INTEGER,
+    title_id VARCHAR(10) NOT NULL UNIQUE,
     title VARCHAR(30),
     PRIMARY KEY(title_id)
 );
 
-CREATE TABLE IF NOT EXISTS dept_manager
+CREATE TABLE IF NOT EXISTS dept_managers
 (
-    employee_no INTEGER NOT NULL,
-    departments_dept_no VARCHAR(10),
-    departments_dept_nam VARCHAR(30),
-    dept_no VARCHAR(10),
-    departments_dept_no INTEGER,
-    PRIMARY KEY(employee_no)
+    dept_no VARCHAR(50) NOT NULL UNIQUE,
+    emp_no VARCHAR(10) NOT NULL UNIQUE,
+    PRIMARY KEY(dept_no, emp_no)
 );
 
-CREATE TABLE IF NOT EXISTS dept_emp
+CREATE TABLE IF NOT EXISTS dept_emps
 (
-    employee_no INTEGER NOT NULL,
-    departments_dept_no VARCHAR(10),
-    departments_dept_nam VARCHAR(30),
-    dept_no VARCHAR(10),
-    PRIMARY KEY(employee_no)
+    emp_no VARCHAR(10) NOT NULL UNIQUE,
+    dept_no VARCHAR(10) NOT NULL UNIQUE,
+    PRIMARY KEY(emp_no, dept_no)
 );
 
 
@@ -63,75 +56,93 @@ ALTER TABLE titles
     MATCH SIMPLE
 ;
     
-ALTER TABLE salary
+ALTER TABLE salarys
     ADD    FOREIGN KEY (employee_no)
     REFERENCES employees(employee_no)
     MATCH SIMPLE
 ;
     
-ALTER TABLE dept_manager
+ALTER TABLE dept_managers
+    ADD    FOREIGN KEY (emp_no)
+    REFERENCES employees(employee_no)
+    MATCH SIMPLE
+;
+    
+ALTER TABLE employees
     ADD    FOREIGN KEY (employee_no)
-    REFERENCES employees(employee_no)
+    REFERENCES salarys(salary)
     MATCH SIMPLE
 ;
     
-ALTER TABLE dept_emp
+ALTER TABLE employees
+    ADD    FOREIGN KEY (employee_title_id)
+    REFERENCES titles(title_id)
+    MATCH SIMPLE
+;
+    
+ALTER TABLE salarys
     ADD    FOREIGN KEY (employee_no)
+    REFERENCES titles(title_id)
+    MATCH SIMPLE
+;
+    
+ALTER TABLE dept_emps
+    ADD    FOREIGN KEY (emp_no)
     REFERENCES employees(employee_no)
     MATCH SIMPLE
 ;
     
-ALTER TABLE dept_manager
-    ADD    FOREIGN KEY (departments_dept_no)
+ALTER TABLE dept_managers
+    ADD    FOREIGN KEY (dept_no)
     REFERENCES departments(dept_no)
     MATCH SIMPLE
 ;
     
-ALTER TABLE titles
-    ADD    FOREIGN KEY (employees_employee_no)
-    REFERENCES employees(employee_no)
-    MATCH SIMPLE
-;
-    
-ALTER TABLE salary
-    ADD    FOREIGN KEY (employees_employee_no)
-    REFERENCES employees(employee_no)
-    MATCH SIMPLE
-;
-    
-ALTER TABLE dept_manager
-    ADD    FOREIGN KEY (departments_dept_no)
+ALTER TABLE dept_emps
+    ADD    FOREIGN KEY (dept_no)
     REFERENCES departments(dept_no)
     MATCH SIMPLE
 ;
     
-ALTER TABLE dept_emp
-    ADD    FOREIGN KEY (departments_dept_no)
-    REFERENCES departments(dept_no)
+ALTER TABLE salarys
+    ADD    FOREIGN KEY (employee_no)
+    REFERENCES titles(title)
     MATCH SIMPLE
 ;
     
-ALTER TABLE dept_manager
+ALTER TABLE salarys
     ADD    FOREIGN KEY (employee_no)
     REFERENCES employees(employee_title_id)
     MATCH SIMPLE
 ;
     
-ALTER TABLE dept_emp
-    ADD    FOREIGN KEY (employee_no)
+ALTER TABLE salarys
+    ADD    FOREIGN KEY (salary)
+    REFERENCES titles(title_id)
+    MATCH SIMPLE
+;
+    
+ALTER TABLE dept_managers
+    ADD    FOREIGN KEY (dept_no)
+    REFERENCES departments(dept_nam)
+    MATCH SIMPLE
+;
+    
+ALTER TABLE dept_emps
+    ADD    FOREIGN KEY (emp_no)
+    REFERENCES departments(dept_no)
+    MATCH SIMPLE
+;
+    
+ALTER TABLE dept_managers
+    ADD    FOREIGN KEY (emp_no)
     REFERENCES employees(employee_title_id)
     MATCH SIMPLE
 ;
     
-ALTER TABLE dept_emp
-    ADD    FOREIGN KEY (departments_dept_nam)
-    REFERENCES departments(dept_nam)
-    MATCH SIMPLE
-;
-    
-ALTER TABLE dept_manager
-    ADD    FOREIGN KEY (departments_dept_nam)
-    REFERENCES departments(dept_nam)
+ALTER TABLE departments
+    ADD    FOREIGN KEY (dept_no)
+    REFERENCES employees(employee_title_id)
     MATCH SIMPLE
 ;
     
